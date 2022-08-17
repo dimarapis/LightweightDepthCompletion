63e9f130faad03a9e8a69b2076271de3a88547ab
dim <dimarapis@hotmaill.com>
2022-08-17 11:32:40 +0200

    Added DecnetModule

--- models/sparse_guided_depth.py
+++ models/sparse_guided_depth.py
@@ -1364,3 +1364,57 @@ class DepthRefinementSmall(nn.Module):
         
         return refined_depth
     
+    
+    
+class DecnetModule(nn.Module):
+    def __init__(self, 
+            pretrained=True,
+            up_features=[64, 32, 16], 
+            inner_features=[64, 32, 16]):
+        super(DecnetModule, self).__init__()
+
+        self.feature_extractor = DualResNet_Backbone(
+                pretrained=pretrained, 
+                features=up_features[0])
+
+        self.up_1 = Guided_Upsampling_Block(in_features=up_features[0],
+                                   expand_features=inner_features[0],
+                                   out_features=up_features[1],
+                                   kernel_size=3,
+                                   channel_attention=True,
+                                   guide_features=4,
+                                   guidance_type="full")
+        self.up_2 = Guided_Upsampling_Block(in_features=up_features[1],
+                                   expand_features=inner_features[1],
+                                   out_features=up_features[2],
+                                   kernel_size=3,
+                                   channel_attention=True,
+                                   guide_features=4,
+                                   guidance_type="full")
+        self.up_3 = Guided_Upsampling_Block(in_features=up_features[2],
+                                   expand_features=inner_features[2],
+                                   out_features=1,
+                                   kernel_size=3,
+                                   channel_attention=True,
+                                   guide_features=4,
+                                   guidance_type="full")
+
+
+    def forward(self, rgb, depth):
+        y = self.feature_extractor(rgb)
+        #print('tara')
+        
+        #rgbd = torch.cat((rgb,depth),1)
+
+        x_half = F.interpolate(rgb, scale_factor=.5)
+        x_quarter = F.interpolate(rgb, scale_factor=.25)
+
+        y = F.interpolate(y, scale_factor=2., mode='bilinear')#, align_corners=True)
+        y = self.up_1(x_quarter, y)
+
+        y = F.interpolate(y, scale_factor=2., mode='bilinear')#,align_corners=True)
+        y = self.up_2(x_half, y)
+
+        y = F.interpolate(y, scale_factor=2., mode='bilinear')#, align_corners=True)
+        y = self.up_3(rgb, y)
+        return y
